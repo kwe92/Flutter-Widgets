7d2749e9e9ce3344b2173e9174d4e60c09e372fe
Kweayon  Clark <Kweayon@gmail.com>
2022-10-13 08:54:00 -0500

    Update
    MaterialApp with route

--- lib/Widgets/MaterialApp/material_app_nav.dart
+++ lib/Widgets/MaterialApp/material_app_nav.dart
@@ -6,36 +6,42 @@ class CustomMaterialAppNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme:
           ThemeData(brightness: Brightness.dark, primaryColor: Colors.orange),
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Home'),
-            ),
-            body: Center(
-                child: GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/about');
-              },
-              child: Container(
-                height: 100,
-                width: 400,
-                margin: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-                color: Colors.blueGrey,
-                child: const Center(
-                    child: FittedBox(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
-                    child: Text(
-                      'Container turned Button, routing to the About Screen',
-                      style: TextStyle(fontSize: 40, color: Colors.white),
-                    ),
-                  ),
-                )),
+          return SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text('Home'),
               ),
-            )),
+              body: Center(
+                  child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/about');
+                },
+                child: Container(
+                  height: 100,
+                  width: 400,
+                  padding: const EdgeInsets.all(8),
+                  margin: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: const Color.fromRGBO(59, 68, 131, 1)),
+                  child: const Center(
+                      child: FittedBox(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                      child: Text(
+                        'Container turned Button, routing to the About Screen',
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                    ),
+                  )),
+                ),
+              )),
+            ),
           );
         },
         '/about': (BuildContext context) {
@@ -46,7 +52,6 @@ class CustomMaterialAppNav extends StatelessWidget {
           );
         }
       },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
