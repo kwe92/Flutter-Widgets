d3b6ddcad9b15a5332b99457180d259bdb833b7c
Kweayon  Clark <Kweayon@gmail.com>
2022-10-14 13:48:30 -0500

    Initial Commit
    Genre Hard coded data

---
+++ lib/Widgets/expansion_list_tile/data/genre_data.dart
@@ -0,0 +1,26 @@
+const List<Map<String, String>> genreJSON = [
+  {
+    "genre": "Isekai",
+    "image": "assets/sololvl.jpeg",
+    "description":
+        "Isekai (Japanese: 異世界, transl. 'different world' or 'otherworld') is a Japanese genre of portal fantasy and science fiction. It includes novels, light novels, films, manga, anime and video games that revolve around a person or people who are transported to and have to survive in another world, such as a fantasy world, virtual world, or parallel universe. Isekai is one of the most popular genres of anime, and Isekai stories share many common tropes – for example, a powerful protagonist who is able to beat most people in the other world by fighting. This plot device typically allows the audience to learn about the new world at the same pace as the protagonist over the course of their quest or lifetime.[1] If the main characters are transported to a game-like world, the genre can overlap with LitRPG."
+  },
+  {
+    "genre": "Slice of Life",
+    "image": "assets/sololvl.jpeg",
+    "description":
+        "Slice of life is a depiction of mundane experiences in art and entertainment.[1] In theater, slice of life refers to naturalism, while in literary parlance it is a narrative technique in which a seemingly arbitrary sequence of events in a character's life is presented, often lacking plot development, conflict and exposition, as well as often having an open ending."
+  },
+  {
+    "genre": "Seinen",
+    "image": "assets/sololvl.jpeg",
+    "description":
+        "Seinen manga (青年漫画) is an editorial category of Japanese comics marketed toward young adult men.[1] In Japanese, the word seinen literally means 'youth', but the term 'seinen manga' is also used to describe the target audience of magazines like Weekly Manga Times and Weekly Manga Goraku which cater specifically to men's interests, and are marketed towards a demographic of young adult men between the ages of 18 and 40. Seinen manga are distinguished from shōnen manga which are for young teen boys, although some seinen manga like xxxHolic share similarities with shōnen manga. Seinen manga can focus on action, politics, science fiction, fantasy, relationships, sports, or comedy. The female equivalent to seinen manga is josei manga."
+  },
+  {
+    "genre": "Harem",
+    "image": "assets/sololvl.jpeg",
+    "description":
+        "Harem (ハーレムもの, hāremumono, 'harem works') is a genre of light novels, manga, anime, hentai, and video games originating in Japan in the 1970s but exploding late 1980s and 1990s with dating simulator games and focused on polygynous or polyandrous relationships, where a protagonist is surrounded by three or more androphilic/gynephilic love interests or sexual partners. Harem works are frequently comedies that rely on self-insertion protagonists allowing the audience to project themselves unto, and on having relatable and interesting ensemble cast of characters. A story featuring a heterosexual male or homosexual female protagonist paired with an all-female/yuri harem series is informally referred to as a female harem or seraglios, while a heterosexual female or gay male protagonist paired with an all-male/yaoi harem series is informally referred to as a male harem, reverse harem, or gyaku hāremu (逆ハーレム). Although originating in Japan, the genre later inspired variants in Western media."
+  }
+];
