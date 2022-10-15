89f28891c27a81855ca588fa4630c4fb605a43f2
Kweayon  Clark <Kweayon@gmail.com>
2022-10-15 14:36:50 -0500

    Update
    Added const layout

---
+++ lib/Widgets/expansion_list_tile/constants/layout.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Sizes {
+  static const p1 = 1.0;
+  static const p2 = 2.0;
+  static const p4 = 4.0;
+  static const p8 = 8.0;
+  static const p12 = 12.0;
+  static const p16 = 16.0;
+  static const p20 = 20.0;
+  static const p24 = 24.0;
+  static const p26 = 26.0;
+  static const p30 = 30.0;
+  static const p32 = 32.0;
+  static const p34 = 34.0;
+  static const p38 = 38.0;
+  static const p40 = 40.0;
+  static const p42 = 42.0;
+}
+
+const gapH1 = SizedBox(height: Sizes.p1);
+const gapH2 = SizedBox(height: Sizes.p2);
+const gapH4 = SizedBox(height: Sizes.p4);
+const gapH8 = SizedBox(height: Sizes.p8);
+const gapH12 = SizedBox(height: Sizes.p12);
+const gapH16 = SizedBox(height: Sizes.p16);
+
+class Fonts {
+  static const main = "Quicksand";
+  static const secondary = "Estonia";
+}
+
+class BackgroundColors {
+  static const main = Color.fromRGBO(121, 172, 179, 1);
+}
