c551db884da1167dd811f2906591474255dbf001
Kweayon  Clark <Kweayon@gmail.com>
2022-10-15 14:19:55 -0500

    Update
    expansion file

--- lib/Widgets/expansion_list_tile/presentation/expansion_tile.dart
+++ lib/Widgets/expansion_list_tile/presentation/expansion_tile.dart
@@ -10,42 +10,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_widgets/Widgets/expansion_list_tile/constants/layout.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/data/genre_data.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/domain/genre.dart';
+import 'package:flutter_widgets/Widgets/expansion_list_tile/presentation/genre_screen.dart';
 
 class WorkingWithExpansionListTile extends StatelessWidget {
-  const WorkingWithExpansionListTile({super.key});
+  WorkingWithExpansionListTile({super.key});
+  final genreList = GenreModel.fromList(genreJSON);
+  GenreModel getGenre(genre) {
+    return genreList.firstWhere((ele) => ele.genre.toLowerCase() == genre);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final genreList = GenreModel.fromList(genreJSON);
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('ExpansionTile Widget'),
-        ),
-        body: ExpansionTile(
-          title: const Text('Anime Genre'),
-          children: <Widget>[
-            ListTile(
-              title: const Text('Isekai'),
-              onTap: () {},
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) {
+          return SafeArea(
+            child: Scaffold(
+              // appBar: AppBar(
+              //   title: const Text('ExpansionTile Widget'),
+              // ),
+              body: Column(
+                children: [
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        const Color.fromRGBO(88, 82, 123, 1),
+                    title: const Text(
+                      'Anime Genre',
+                      style: TextStyle(fontSize: Sizes.p20),
+                    ),
+                    children: <Widget>[
+                      ListTile(
+                        title: const Text('Isekai'),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/isekai');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('Slice of Life'),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/sol');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('Seinen'),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/seinen');
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('Harem'),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/harem');
+                        },
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Image.asset(
+                      fit: BoxFit.fitHeight,
+                      'assets/seinen.webp',
+                    ),
+                  ),
+                ],
+              ),
             ),
-            ListTile(
-              title: const Text('Slice of Life'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: const Text('Seinen'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: const Text('Harem'),
-              onTap: () {},
-            )
-          ],
-        ),
-      ),
+          );
+        },
+        '/isekai': (BuildContext context) {
+          return GenreScreen(genreEntity: getGenre('isekai'));
+        },
+        '/sol': (BuildContext context) {
+          return GenreScreen(genreEntity: getGenre('slice of life'));
+        },
+        '/seinen': (BuildContext context) {
+          return GenreScreen(genreEntity: getGenre('seinen'));
+        },
+        '/harem': (BuildContext context) {
+          return GenreScreen(genreEntity: getGenre('harem'));
+        },
+      },
     );
   }
 }
