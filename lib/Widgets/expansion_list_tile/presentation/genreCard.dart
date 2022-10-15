3868e8254e054f4582bec860e05b99d2ba25c79d
Kweayon  Clark <Kweayon@gmail.com>
2022-10-15 14:39:28 -0500

    Update
    Created a custom genre card

--- lib/Widgets/expansion_list_tile/presentation/genreCard.dart
+++ lib/Widgets/expansion_list_tile/presentation/genreCard.dart
@@ -1,30 +1,158 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_widgets/Widgets/expansion_list_tile/constants/layout.dart';
 
 class GenreCard extends StatelessWidget {
   const GenreCard(
-      {required this.genre,
+      { //required this.genre,
       required this.image,
       required this.description,
       super.key});
-  final String genre;
+  //final String genre;
   final String image;
   final String description;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.grey[300],
-      elevation: 0.0,
-      margin: const EdgeInsets.all(0),
+      //color: Colors.grey[300],
+      elevation: 12.0,
+      margin: const EdgeInsets.all(6),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(genre),
-          Image.asset(image),
-          Text(description),
+          // Padding(
+          //   padding: const EdgeInsets.fromLTRB(8, 8, 0, 0),
+          //   child: Center(
+          //     child: Text(
+          //       genre,
+          // style: const TextStyle(
+          //   fontSize: 52,
+          //   fontFamily: Fonts.secondary,
+          // ),
+          //     ),
+          //   ),
+          // ),
+          // gapH4,
+          SizedBox(
+            height: 400,
+            //width: 30,
+            child: AspectRatio(
+              aspectRatio: 4 / 3,
+              child: Image.asset(image, fit: BoxFit.fill),
+            ),
+          ),
+          gapH8,
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Description:',
+                      style: TextStyle(
+                          fontSize: Sizes.p26, fontFamily: Fonts.main),
+                    ),
+                    gapH8,
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(Sizes.p16, 0, 0, 0),
+                      child: Text(
+                        description,
+                        style: const TextStyle(
+                            fontSize: Sizes.p26, fontFamily: Fonts.main),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+// Padding(
+//             padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+            // child: Text(
+            //   'Description:',
+            //   textAlign: TextAlign.left,
+            //   style: TextStyle(fontSize: 26.0),
+            // ),
+//           ),
+//           gapH8,
+//           Expanded(
+//             child: SingleChildScrollView(
+//               child: Padding(
+//                 padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+//                 child: Text(
+//                   description,
+//                   style: const TextStyle(fontSize: 20.0),
+//                   textAlign: TextAlign.left,
+//                 ),
+//               ),
+//             ),
+//           ),
+
+////////////////////////
+
+//  const Padding(
+//             padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+//             child: Text(
+//               'Description:',
+//               textAlign: TextAlign.left,
+//               style: TextStyle(fontSize: 26.0),
+//             ),
+//           ),
+//           gapH8,
+          // Expanded(
+          //   child: SingleChildScrollView(
+          //     child: Padding(
+          //       padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+          //       child: Text(
+          //         description,
+          //         style: const TextStyle(fontSize: 20.0),
+          //         textAlign: TextAlign.left,
+          //       ),
+          //     ),
+          //   ),
+//           ),
+
+// Sliver Scroll
+
+// Expanded(
+//             child: CustomScrollView(
+//               slivers: [
+//                 SliverList(
+//                   delegate: SliverChildListDelegate(
+//                     <Widget>[
+//                       Padding(
+//                         padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+//                         child: Text(
+//                           'Description:',
+//                           textAlign: TextAlign.left,
+//                           style: TextStyle(fontSize: 26.0),
+//                         ),
+//                       ),
+//                       gapH8,
+//                       Expanded(
+//                         child: SingleChildScrollView(
+//                           child: Padding(
+//                             padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+//                             child: Text(
+//                               description,
+//                               style: const TextStyle(fontSize: 20.0),
+//                               textAlign: TextAlign.left,
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 )
+//               ],
+//             ),
+//           )
