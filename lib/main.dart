4947685e935ab978a5d673836e7900492b49116b
Kweayon  Clark <Kweayon@gmail.com>
2022-10-16 19:33:39 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_widgets/Widgets/WorkingWithAsyncData/Riverpod/working_with_riverpod.dart';
 import 'package:flutter_widgets/Widgets/choice_chip.dart';
+import 'package:flutter_widgets/Widgets/container_widget.dart';
 import 'package:flutter_widgets/Widgets/dot_adaptive.dart';
 import 'package:flutter_widgets/Widgets/MaterialApp/material_app_nav.dart';
 import 'package:flutter_widgets/Widgets/MaterialApp/material_app_widget_no_scaffold.dart';
 import 'package:flutter_widgets/Widgets/builder_widget.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/data/genre_data.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/domain/genre.dart';
-import 'package:flutter_widgets/Widgets/expansion_list_tile/presentation/expansion_tile.dart';
+import 'package:flutter_widgets/Widgets/expansion_list_tile/presentation/genre_home_screen.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/presentation/genreCard.dart';
 import 'package:flutter_widgets/Widgets/fitted_box_widget.dart';
 import 'package:flutter_widgets/Widgets/Hero_Widget/screen_one.dart';
 import 'package:flutter_widgets/Widgets/separation_encapsulation/conuter_app/counter_app.dart';
 import 'package:flutter_widgets/Widgets/separation_encapsulation/conuter_app/review/counter_app2.dart';
+import 'package:flutter_widgets/Widgets/show_date_picker.dart';
 import 'package:flutter_widgets/Widgets/show_search_widget.dart';
+import 'package:flutter_widgets/Widgets/show_time_picker.dart';
 import 'package:flutter_widgets/Widgets/sliver_app_bar.dart';
 import 'package:flutter_widgets/Widgets/stepper_widget.dart';
 import 'package:flutter_widgets/Widgets/WorkingWithAsyncData/stream_builder.dart';
@@ -34,25 +37,23 @@ import 'package:flutter_widgets/Widgets/wrap_widget.dart';
 // CounterApp()
 // WorkingWithSliverAppBar()
 // WorkingWithWrap()
-// WorkingWithExpansionListTile()
+// WorkingWithExpansionListTile() MA
+// WorkingWithContainer()
+// WorkingWithTimePicker()
+// WorkingWithDatePicker()
+//
+//
+//
+//
+//
+//
+//
+//
 // nl
 void main() {
-  runApp(WorkingWithExpansionListTile());
+  runApp(WorkingWithExpansionListTile()
+      //   const MaterialApp(
+      //   home: null,
+      // )
+      );
 }
-
-//  runApp(
-    // MaterialApp(
-    //   theme: ThemeData(brightness: Brightness.dark),
-    //   home: SafeArea(
-    //     child: Scaffold(
-    //       // appBar: AppBar(
-    //       //   title: const Text('Designing Genre Card'),
-    //       // ),
-          // body: GenreCard(
-          //     genre: genreEntity.genre,
-          //     image: genreEntity.image,
-          //     description: genreEntity.description),
-    //     ),
-    //   ),
-    // ),
-//   );
