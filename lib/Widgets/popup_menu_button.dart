bddeed2e1d59db469a55e56b4ff5e688d63c70f8
Kweayon  Clark <Kweayon@gmail.com>
2022-10-17 21:12:51 -0500

    inital commit

---
+++ lib/Widgets/popup_menu_button.dart
@@ -0,0 +1,57 @@
+// Working With PopUpMenuButton()
+//    - Create a pop up menu in the app bar
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class WorkingWithPopUpMenuButton extends StatefulWidget {
+  const WorkingWithPopUpMenuButton({super.key});
+
+  @override
+  State<WorkingWithPopUpMenuButton> createState() =>
+      _WorkingWithPopUpMenuButtonState();
+}
+
+class _WorkingWithPopUpMenuButtonState
+    extends State<WorkingWithPopUpMenuButton> {
+  final String title0 = 'Working With PopupMenuButton';
+  final String title1 = 'Title 2';
+  final String title2 = 'Title 3';
+  late String mutableTitle = title0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: FittedBox(child: Text(mutableTitle)),
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) => [
+                PopupMenuItem(
+                  value: title0,
+                  child: const Text('Title 1'),
+                ),
+                PopupMenuItem(
+                  value: title1,
+                  child: Text(title1),
+                ),
+                PopupMenuItem(
+                  value: title2,
+                  child: Text(title2),
+                ),
+              ],
+              onSelected: (String newValue) {
+                setState(() {
+                  mutableTitle = newValue;
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
