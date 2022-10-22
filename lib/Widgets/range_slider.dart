e8a1cd566a83388e81ca3574247143d5fa808dbe
Kweayon  Clark <Kweayon@gmail.com>
2022-10-21 23:06:49 -0500

    Range Slider
    Working With Range Slider

---
+++ lib/Widgets/range_slider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class WorknigWithRangeSlider extends StatefulWidget {
+  const WorknigWithRangeSlider({super.key});
+
+  @override
+  State<WorknigWithRangeSlider> createState() => _WorknigWithRangeSliderState();
+}
+
+class _WorknigWithRangeSliderState extends State<WorknigWithRangeSlider> {
+  RangeValues values = const RangeValues(0, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    RangeLabels labels =
+        RangeLabels(values.start.toString(), values.end.toString());
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Range Slider Widget'),
+        ),
+        body: Center(
+          child: RangeSlider(
+            values: values,
+            divisions: 8,
+            labels: labels,
+            onChanged: (newValues) {
+              setState(() {
+                values = newValues;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
