5576b3e71483e8f6b983678d34fd245dc0e8d8af
Kweayon  Clark <Kweayon@gmail.com>
2022-10-22 12:29:17 -0500

    Update
    Added a Consumer Widget and a Provider for the title to practice working with Consumer and Provider classes

--- lib/Widgets/expansion_list_tile/presentation/genre_home_screen.dart
+++ lib/Widgets/expansion_list_tile/presentation/genre_home_screen.dart
@@ -8,13 +8,13 @@
 //  - Makes adding icons, callbacks, and text easy
 //  - leading and trailing icons, a column of texts inbetween
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_widgets/Widgets/container/container_widget.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/constants/layout.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/data/genre_data.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/domain/genre.dart';
+import 'package:flutter_widgets/Widgets/expansion_list_tile/presentation/controllers/providers.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/presentation/genre_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class WorkingWithExpansionListTile extends StatelessWidget {
   WorkingWithExpansionListTile({super.key});
@@ -25,72 +25,79 @@ class WorkingWithExpansionListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) {
-          return SafeArea(
-            child: Scaffold(
-              body: ListView(
-                children: [
-                  ExpansionTile(
-                    collapsedBackgroundColor: BackgroundColors.secondary,
-                    title: const Text(
-                      'Anime Genre',
-                      style: TextStyle(fontSize: Sizes.p20),
+    return ProviderScope(
+      child: MaterialApp(
+        theme: ThemeData(brightness: Brightness.dark),
+        routes: <String, WidgetBuilder>{
+          '/': (BuildContext context) {
+            return SafeArea(
+              child: Scaffold(
+                body: ListView(
+                  children: [
+                    ExpansionTile(
+                      collapsedBackgroundColor: BackgroundColors.secondary,
+                      title: Consumer(
+                        builder: (context, ref, child) {
+                          final String title = ref.read(titleProvider);
+                          return Text(
+                            title,
+                            style: const TextStyle(fontSize: Sizes.p20),
+                          );
+                        },
+                      ),
+                      children: <Widget>[
+                        ListTile(
+                          title: const Text('Isekai'),
+                          onTap: () {
+                            Navigator.pushNamed(context, '/isekai');
+                          },
+                        ),
+                        ListTile(
+                          title: const Text('Slice of Life'),
+                          onTap: () {
+                            Navigator.pushNamed(context, '/sol');
+                          },
+                        ),
+                        ListTile(
+                          title: const Text('Seinen'),
+                          onTap: () {
+                            Navigator.pushNamed(context, '/seinen');
+                          },
+                        ),
+                        ListTile(
+                          title: const Text('Harem'),
+                          onTap: () {
+                            Navigator.pushNamed(context, '/harem');
+                          },
+                        ),
+                      ],
                     ),
-                    children: <Widget>[
-                      ListTile(
-                        title: const Text('Isekai'),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/isekai');
-                        },
-                      ),
-                      ListTile(
-                        title: const Text('Slice of Life'),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/sol');
-                        },
-                      ),
-                      ListTile(
-                        title: const Text('Seinen'),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/seinen');
-                        },
-                      ),
-                      ListTile(
-                        title: const Text('Harem'),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/harem');
-                        },
-                      ),
-                    ],
-                  ),
-                  buildWidetColumn()
-                  // Expanded(
-                  //   child: Image.asset(
-                  //     fit: BoxFit.fitHeight,
-                  //     'assets/seinen.webp',
-                  //   ),
-                  // ),
-                ],
+                    buildWidetColumn()
+                    // Expanded(
+                    //   child: Image.asset(
+                    //     fit: BoxFit.fitHeight,
+                    //     'assets/seinen.webp',
+                    //   ),
+                    // ),
+                  ],
+                ),
               ),
-            ),
-          );
+            );
+          },
+          '/isekai': (BuildContext context) {
+            return GenreScreen(genreEntity: getGenre('isekai'));
+          },
+          '/sol': (BuildContext context) {
+            return GenreScreen(genreEntity: getGenre('slice of life'));
+          },
+          '/seinen': (BuildContext context) {
+            return GenreScreen(genreEntity: getGenre('seinen'));
+          },
+          '/harem': (BuildContext context) {
+            return GenreScreen(genreEntity: getGenre('harem'));
+          },
         },
-        '/isekai': (BuildContext context) {
-          return GenreScreen(genreEntity: getGenre('isekai'));
-        },
-        '/sol': (BuildContext context) {
-          return GenreScreen(genreEntity: getGenre('slice of life'));
-        },
-        '/seinen': (BuildContext context) {
-          return GenreScreen(genreEntity: getGenre('seinen'));
-        },
-        '/harem': (BuildContext context) {
-          return GenreScreen(genreEntity: getGenre('harem'));
-        },
-      },
+      ),
     );
   }
 }
