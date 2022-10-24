6f26a2f9d6816e637040435c0023ff6f469b217f
Kweayon  Clark <Kweayon@gmail.com>
2022-10-23 20:41:59 -0500

    Update Neumorphic
    madea custom neumorphic button

---
+++ lib/Widgets/neumorphism/separation_encapsulation/conuter_app/review/counter_app/presentation/decrement_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../constants/layout.dart';
+import 'eleveated_button.dart';
+import 'providers/count_provider.dart';
+
+class CounterDecrementor extends StatelessWidget {
+  CounterDecrementor({required this.onPressed, super.key});
+  final VoidCallback onPressed;
+  final Widget text = const Text('Decrement');
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        final count = ref.read(countProvider.state).state;
+        final returnedWidget = count > 0
+            ? CustomNeumorphicButton(
+                onPressed: onPressed,
+                color: AppColor.main,
+                child: text,
+              )
+            : CustomNeumorphicButton(
+                onPressed: () {}, color: Colors.grey, child: text);
+        return returnedWidget;
+      },
+    );
+  }
+}
