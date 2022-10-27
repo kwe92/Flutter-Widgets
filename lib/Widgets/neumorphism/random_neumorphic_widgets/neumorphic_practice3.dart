13e607299005806e22120a6f4a85bbda98a7c545
Kweayon  Clark <Kweayon@gmail.com>
2022-10-27 11:50:18 -0500

    WE LEVELED UP! Better Buttons
    Figured out how to create a custom neumorphic button that can be pressed and changed when pressed to inset and also how to make our custom neumorphic button update the UI AFTER the inset is finshed and not before

--- lib/Widgets/neumorphism/random_neumorphic_widgets/neumorphic_practice3.dart
+++ lib/Widgets/neumorphism/random_neumorphic_widgets/neumorphic_practice3.dart
@@ -5,7 +5,9 @@ import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_widgets/Widgets/neumorphism/random_neumorphic_widgets/providers.dart';
 
-class MyButton extends ConsumerWidget {
+// A neumorphic button that updates UI after the button is pressed and not before
+
+class MyButton extends StatefulWidget {
   const MyButton(
       {required this.color,
       required this.textColor,
@@ -14,7 +16,6 @@ class MyButton extends ConsumerWidget {
       required this.fontSize,
       Key? key})
       : super(key: key);
-
   final Color? color;
   final Color? textColor;
   final String buttonText;
@@ -22,28 +23,45 @@ class MyButton extends ConsumerWidget {
   final void Function()? buttontapped;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    bool isPressed = ref.watch(isPressedProvider.state).state;
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  bool isPressed = false;
+  @override
+  Widget build(BuildContext context) {
     //final Color backgroundColor = Colors.grey[100]!;
-    final Offset distance =
-        isPressed ? const Offset(6, 6) : const Offset(12, 12);
+    final Offset distance = isPressed ? const Offset(5, 5) : const Offset(8, 8);
     final double blur = isPressed ? 5 : 15.0;
 
     return GestureDetector(
-      onTap: buttontapped,
+      onTap: () async {
+        await () async {
+          setState(() {
+            isPressed = !isPressed;
+          });
+          await Future.delayed(const Duration(milliseconds: 500));
+          setState(() {
+            isPressed = !isPressed;
+          });
+        }();
+
+        // Callback is called after button tap
+        widget.buttontapped!();
+      },
       child: AnimatedContainer(
         duration: const Duration(microseconds: 50),
         height: 200,
         width: 200,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
-            color: color,
+            color: widget.color,
             boxShadow: <BoxShadow>[
               // right shadow
               BoxShadow(
                   blurRadius: blur,
                   offset: distance,
-                  color: Colors.grey[500]!,
+                  color: Colors.grey[300]!,
                   inset: isPressed),
               // left shadow
               BoxShadow(
@@ -54,11 +72,11 @@ class MyButton extends ConsumerWidget {
             ]),
         child: Center(
           child: Text(
-            buttonText,
+            widget.buttonText,
             style: TextStyle(
-                fontSize: fontSize,
+                fontSize: widget.fontSize,
                 fontWeight: FontWeight.bold,
-                color: textColor),
+                color: widget.textColor),
           ),
         ),
       ),
@@ -75,28 +93,109 @@ class NeumorphicPractice3 extends ConsumerStatefulWidget {
 }
 
 class _NeumorphicPractice3State extends ConsumerState<NeumorphicPractice3> {
-  //bool isPressed = false;
-  void pressButton() async {
-    ref.read(isPressedProvider.state).state = true;
-    await Future.delayed(const Duration(milliseconds: 50));
-    ref.read(isPressedProvider.state).state = false;
+  int num = 5;
+  void pressButton() {
+    setState(() {
+      num = -num;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     //bool isPressed = ref.watch(isPressedProvider.state).state;
     final Color backgroundColor = Colors.grey[100]!;
+
     return SafeArea(
       child: Scaffold(
           backgroundColor: backgroundColor,
           body: Center(
-            child: MyButton(
-                color: backgroundColor,
-                textColor: Colors.black,
-                buttonText: '42',
-                buttontapped: pressButton,
-                fontSize: 26),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  num.toString(),
+                  style: const TextStyle(
+                      fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                MyButton(
+                    color: backgroundColor,
+                    textColor: Colors.black,
+                    buttonText: '- / +',
+                    buttontapped: pressButton,
+                    fontSize: 26),
+              ],
+            ),
           )),
     );
   }
 }
+
+// Note: Button With a provider for global state management 
+
+// class MyButton extends ConsumerWidget {
+//   const MyButton(
+//       {required this.color,
+//       required this.textColor,
+//       required this.buttonText,
+//       required this.buttontapped,
+//       required this.fontSize,
+//       Key? key})
+//       : super(key: key);
+
+//   final Color? color;
+//   final Color? textColor;
+//   final String buttonText;
+//   final double fontSize;
+//   final void Function()? buttontapped;
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     bool isPressed = false;
+//     //ref.watch(isPressedProvider.state).state;
+//     //final Color backgroundColor = Colors.grey[100]!;
+//     final Offset distance =
+//         isPressed ? const Offset(6, 6) : const Offset(12, 12);
+//     final double blur = isPressed ? 5 : 15.0;
+
+//     return GestureDetector(
+//       onTap: () {
+//         buttontapped!();
+//         isPressed = !isPressed;
+//       },
+//       child: AnimatedContainer(
+//         duration: const Duration(microseconds: 50),
+//         height: 200,
+//         width: 200,
+//         decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(30),
+//             color: color,
+//             boxShadow: <BoxShadow>[
+//               // right shadow
+//               BoxShadow(
+//                   blurRadius: blur,
+//                   offset: distance,
+//                   color: Colors.grey[500]!,
+//                   inset: isPressed),
+//               // left shadow
+//               BoxShadow(
+//                   blurRadius: blur,
+//                   offset: -distance,
+//                   color: Colors.white,
+//                   inset: isPressed)
+//             ]),
+//         child: Center(
+//           child: Text(
+//             buttonText,
+//             style: TextStyle(
+//                 fontSize: fontSize,
+//                 fontWeight: FontWeight.bold,
+//                 color: textColor),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
