aaa2f4eff07e9cb94ced621b51c60be83ab83f6f
Kweayon  Clark <Kweayon@gmail.com>
2022-10-27 11:08:10 -0500

    Neumorphic Practice

---
+++ lib/Widgets/neumorphism/random_neumorphic_widgets/neumorphic_practice2.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+
+class NeumorphicPractice2 extends StatefulWidget {
+  const NeumorphicPractice2({super.key});
+
+  @override
+  State<NeumorphicPractice2> createState() => _NeumorphicPractice2State();
+}
+
+class _NeumorphicPractice2State extends State<NeumorphicPractice2> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final Color backgroundColor = Colors.grey[100]!;
+    final Offset distance =
+        isPressed ? const Offset(6, 6) : const Offset(12, 12);
+    final double blur = isPressed ? 5 : 15.0;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: backgroundColor,
+        body: Center(
+          child: GestureDetector(
+            onTap: () async {
+              setState(() {
+                isPressed = !isPressed;
+              });
+              await Future.delayed(const Duration(milliseconds: 50));
+              setState(() {
+                isPressed = !isPressed;
+              });
+            },
+            child: AnimatedContainer(
+              duration: const Duration(microseconds: 50),
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: backgroundColor,
+                  boxShadow: <BoxShadow>[
+                    // right shadow
+                    BoxShadow(
+                        blurRadius: blur,
+                        offset: distance,
+                        color: Colors.grey[500]!,
+                        inset: isPressed),
+                    // left shadow
+                    BoxShadow(
+                        blurRadius: blur,
+                        offset: -distance,
+                        color: Colors.white,
+                        inset: isPressed)
+                  ]),
+              child: const Center(
+                child: Text(
+                  '8',
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
