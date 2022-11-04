0758fd0c4723b92e286284a4f524ea685f904a4f
Kweayon  Clark <Kweayon@gmail.com>
2022-11-04 15:09:31 -0500

    Responsive App Practice
    Learning how to build responsive apps

---
+++ lib/ResponsiveDesignExamples/responsive_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/ResponsiveDesignExamples/working_with_media_query_basic_example.dart';
+
+class ResponsiveApp extends StatelessWidget {
+  const ResponsiveApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: HomePage(),
+    );
+  }
+}
