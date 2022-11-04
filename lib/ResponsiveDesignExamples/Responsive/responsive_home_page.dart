cda942f7c8c57172a420c79e021248b4b2d95ed6
Kweayon  Clark <Kweayon@gmail.com>
2022-11-04 16:54:44 -0500

    Responsive Layout
    Added the responsive layout home page

---
+++ lib/ResponsiveDesignExamples/Responsive/responsive_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveHomePage extends StatefulWidget {
+  const ResponsiveHomePage({super.key});
+
+  @override
+  State<ResponsiveHomePage> createState() => _ResponsiveHomePageState();
+}
+
+class _ResponsiveHomePageState extends State<ResponsiveHomePage> {
+  @override
+  Widget build(BuildContext contxt) {
+    return SafeArea(child: Scaffold());
+  }
+}
