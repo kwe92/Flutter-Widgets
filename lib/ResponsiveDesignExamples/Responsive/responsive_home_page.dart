2e67f8abd098cf292ee38f92bd84590b05158d13
Kweayon  Clark <Kweayon@gmail.com>
2022-11-04 21:20:46 -0500

    Added ResponsiveLayout Widget

--- lib/ResponsiveDesignExamples/Responsive/responsive_home_page.dart
+++ lib/ResponsiveDesignExamples/Responsive/responsive_home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/ResponsiveDesignExamples/Responsive/desktop_body.dart';
+import 'package:flutter_widgets/ResponsiveDesignExamples/Responsive/mobile_body.dart';
+
+import 'responsive_layout.dart';
 
 //TODO: After creating the inital Scaffolds and displaying them for Mobile & Desktop Pass the ResponsiveLayout widget to the Scaffold with the appropriate widget passed in
 
@@ -12,7 +16,10 @@ class ResponsiveHomePage extends StatefulWidget {
 class _ResponsiveHomePageState extends State<ResponsiveHomePage> {
   @override
   Widget build(BuildContext contxt) {
-    return SafeArea(child: Scaffold());
-    // ResponsiveLayout(mobileBody: , desktopBody: );
+    //return SafeArea(child: Scaffold());
+    return const ResponsiveLayout(
+      mobileBody: MyMobileBody(),
+      desktopBody: MyDesktopBody(),
+    );
   }
 }
