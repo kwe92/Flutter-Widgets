9b7873c1061cfdccb75acc50d89ae5cca1757f66
Kweayon  Clark <Kweayon@gmail.com>
2022-11-04 21:21:17 -0500

    Responsive Mobile App Body

--- lib/ResponsiveDesignExamples/Responsive/mobile_body.dart
+++ lib/ResponsiveDesignExamples/Responsive/mobile_body.dart
@@ -1,1 +1,46 @@
 //TODO: Create a mobile body Scaffold background color deepPurple 300
+
+import 'package:flutter/material.dart';
+
+class MyMobileBody extends StatelessWidget {
+  const MyMobileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      appBar: AppBar(
+        title: const Text('M O B I L E'),
+      ),
+      body: Column(
+        children: <Widget>[
+          // Video
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                color: Colors.deepPurple[400],
+              ),
+            ),
+          ),
+
+          // List of other videos and comments
+          Expanded(
+            child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (BuildContext context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 115,
+                      color: Colors.deepPurple[300],
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
