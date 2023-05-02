fe495777278b601d6ab1b6d910fa855c2cfb16ea
Kwe <Kweayon@gmail.com>
2023-05-01 23:27:55 -0500

    Extended timer and Added high knee background image

--- lib/Widgets/countdown_timer.dart
+++ lib/Widgets/countdown_timer.dart
@@ -54,56 +54,68 @@ class _ProgressIndicatorExampleState extends State<ProgressIndicatorExample>
 
   @override
   Widget build(BuildContext context) {
-    const double sideLength = 230;
+    const double sideLength = 275;
     const double indicatorCircleSideLength = sideLength / 2 - 15;
 
-    return Scaffold(
-      backgroundColor: Colors.grey[400],
-      body: Center(
-        child: Stack(
-          children: <Widget>[
-            Center(
-              child: SizedBox(
-                width: indicatorCircleSideLength,
-                height: indicatorCircleSideLength,
-                child: CircularProgressIndicator(
-                  color: Colors.blue,
-                  strokeWidth: indicatorCircleSideLength,
-                  value: controller.value,
-                  semanticsLabel: 'Circular progress indicator',
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[400],
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://30dayfitness.app/static/eb7d2c345fbc0b90ae9d17f1444e67f7/shutterstock_1517608859.jpg'),
+                    //  AssetImage('assets/high_knees.jpeg'),
+                    fit: BoxFit.fitHeight,
+                  ),
                 ),
               ),
-            ),
-            Center(
-              child: Container(
-                height: sideLength,
-                width: sideLength,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF0E3311).withOpacity(
-                    0.0,
+              Center(
+                child: SizedBox(
+                  width: indicatorCircleSideLength,
+                  height: indicatorCircleSideLength,
+                  child: CircularProgressIndicator(
+                    color: Colors.blue,
+                    strokeWidth: indicatorCircleSideLength,
+                    value: controller.value,
+                    semanticsLabel: 'Circular progress indicator',
                   ),
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 2.25,
-                  ),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(
-                      sideLength / 2,
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: sideLength,
+                  width: sideLength,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF0E3311).withOpacity(
+                      0.0,
+                    ),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2.25,
+                    ),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(
+                        sideLength / 2,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Center(
-              child: Text(
-                (controller.value * 20).toStringAsFixed(0),
-                style: const TextStyle(
-                  fontSize: 52,
-                  color: Colors.white,
+              Center(
+                child: Text(
+                  (controller.value * 20).toStringAsFixed(0),
+                  style: const TextStyle(
+                    fontSize: 52,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
