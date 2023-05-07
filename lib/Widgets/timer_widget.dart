78d2aecc8d839e149cad399964336754b86c5f4d
Kwe <Kweayon@gmail.com>
2023-05-07 07:51:05 -0500

    Comment out

--- lib/Widgets/timer_widget.dart
+++ lib/Widgets/timer_widget.dart
@@ -1,41 +1,40 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_widgets/Widgets/progress_indicator.dart';
+// import 'package:flutter/material.dart';
 
-class TimerWidget extends StatelessWidget {
-  const TimerWidget({super.key});
+// class TimerWidget extends StatelessWidget {
+//   const TimerWidget({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Timer Widget'),
-        ),
-        body: const Center(
-          child: TransparentContainer(),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Timer Widget'),
+//         ),
+//         body: const Center(
+//           child: TransparentContainer(),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class TransparentContainer extends StatelessWidget {
-  const TransparentContainer({super.key});
+// class TransparentContainer extends StatelessWidget {
+//   const TransparentContainer({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    const double sideLength = 305;
-    return Container(
-      height: sideLength,
-      width: sideLength,
-      decoration: BoxDecoration(
-        color: const Color(0xFF0E3311).withOpacity(0.0),
-        border: Border.all(color: Colors.purple),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(sideLength / 2),
-        ),
-      ),
-      child: const CountDownTimer(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     const double sideLength = 305;
+//     return Container(
+//       height: sideLength,
+//       width: sideLength,
+//       decoration: BoxDecoration(
+//         color: const Color(0xFF0E3311).withOpacity(0.0),
+//         border: Border.all(color: Colors.purple),
+//         borderRadius: const BorderRadius.all(
+//           Radius.circular(sideLength / 2),
+//         ),
+//       ),
+//       child: const CountDownTimer(),
+//     );
+//   }
+// }
