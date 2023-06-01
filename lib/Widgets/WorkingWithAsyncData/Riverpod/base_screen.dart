fac42de5429914c199f25c939d4dde6bf3930d62
Kwe <Kweayon@gmail.com>
2023-06-01 13:23:14 -0500

    Working with Async Data

--- lib/Widgets/WorkingWithAsyncData/Riverpod/base_screen.dart
+++ lib/Widgets/WorkingWithAsyncData/Riverpod/base_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class BaseScreen extends StatelessWidget {
   const BaseScreen({super.key, required this.body});
