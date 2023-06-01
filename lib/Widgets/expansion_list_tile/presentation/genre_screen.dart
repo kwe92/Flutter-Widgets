b725df4f66d56e531c6a2460c1a93599a23e61e3
Kwe <Kweayon@gmail.com>
2023-06-01 13:22:15 -0500

    Update

--- lib/Widgets/expansion_list_tile/presentation/genre_screen.dart
+++ lib/Widgets/expansion_list_tile/presentation/genre_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/constants/layout.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/domain/genre.dart';
 import 'package:flutter_widgets/Widgets/expansion_list_tile/presentation/genreCard.dart';
