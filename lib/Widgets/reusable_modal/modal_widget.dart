5afe2dada6bdcc97a36698a6950f0473e1cc13a3
Kwe <Kweayon@gmail.com>
2023-06-01 13:24:37 -0500

    Custom Model Widget View

---
+++ lib/Widgets/reusable_modal/modal_widget.dart
@@ -0,0 +1,34 @@
+import "package:flutter/material.dart";
+import "package:flutter_widgets/Widgets/reusable_modal/modal.dart";
+
+class ModalWidget extends StatelessWidget {
+  const ModalWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Working with Modals"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await Modal.careNavigationPopup<bool>(
+              context,
+              parameters: CareNavigationPopupParameters(
+                title: 'Are you sure you want to quit?',
+                subtitle: 'Your progress will be lost.',
+                defaultReturn: false,
+                options: {
+                  'Cancel': false,
+                  'Yes, Exit': true,
+                },
+              ),
+            );
+          },
+          child: const Text("Show Modal"),
+        ),
+      ),
+    );
+  }
+}
