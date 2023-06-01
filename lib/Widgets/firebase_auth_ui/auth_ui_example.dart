47d1d811337dd8accad7fa553aee7dcdd64cb655
Kwe <Kweayon@gmail.com>
2023-06-01 13:22:22 -0500

    Update

--- lib/Widgets/firebase_auth_ui/auth_ui_example.dart
+++ lib/Widgets/firebase_auth_ui/auth_ui_example.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 class SignInExample extends StatelessWidget {
+  const SignInExample({super.key});
+
   @override
   Widget build(BuildContext context) {
     final providers = [EmailAuthProvider()];
