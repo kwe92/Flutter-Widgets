faa470195557ec8455f49c3a3e5a39ef490ce47a
Kwe <Kweayon@gmail.com>
2023-07-17 17:11:51 -0500

    working with gridview

---
+++ lib/Widgets/grid_view_list_generator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WorkingWithGridView extends StatelessWidget {
+  const WorkingWithGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Working WIth Gridview'),
+          ),
+        ),
+      );
+}
