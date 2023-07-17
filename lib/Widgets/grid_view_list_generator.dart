6a387da14573074a7aef75811cc4bfc6ea6f6d0c
Kwe <Kweayon@gmail.com>
2023-07-17 17:26:01 -0500

    inital grid view

--- lib/Widgets/grid_view_list_generator.dart
+++ lib/Widgets/grid_view_list_generator.dart
@@ -9,6 +9,29 @@ class WorkingWithGridView extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Working WIth Gridview'),
           ),
+          body: GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              ...List.generate(
+                20,
+                (int index) => Center(
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: ColoredBox(
+                      color: Colors.purple,
+                      child: Center(
+                        child: Text(
+                          'Index: $index',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       );
 }
