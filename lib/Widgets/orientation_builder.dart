96ca783871186dd79c5d26cf60182fa0599319f7
Kwe <Kweayon@gmail.com>
2023-07-17 17:55:32 -0500

    inital commit

---
+++ lib/Widgets/orientation_builder.dart
@@ -0,0 +1,11 @@
+// TODO: Create a lesson on Orientation Builder
+
+import 'package:flutter/material.dart';
+
+class WorkingWithOrientationBuilder extends StatelessWidget {
+  const WorkingWithOrientationBuilder({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
