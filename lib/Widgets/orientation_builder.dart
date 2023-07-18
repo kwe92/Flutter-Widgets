076af53829d91bae91370269758dc7d266684de3
Kwe <Kweayon@gmail.com>
2023-07-18 12:54:23 -0500

    update: extended lesson

--- lib/Widgets/orientation_builder.dart
+++ lib/Widgets/orientation_builder.dart
@@ -8,11 +8,26 @@ class WorkingWithOrientationBuilder extends StatelessWidget {
   const WorkingWithOrientationBuilder({super.key});
   @override
   Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Wsorking With Orientation Builder'),
+        ),
+        body: const _CustomGridView(),
+      ),
+    );
+  }
+}
+
+class _CustomGridView extends StatelessWidget {
+  const _CustomGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     final List generatedWidgets = List.generate(
       10,
       generateColoredBox,
     );
-
     return GridView.count(
       crossAxisCount: 2,
       children: <Widget>[
@@ -26,11 +41,15 @@ typedef WidgetGeneratorCallback = Widget Function(
   int index,
 );
 
-final WidgetGeneratorCallback generateColoredBox = (int index) => SizedBox(
-      child: ColoredBox(
-        color: Colors.purple,
-        child: Text(
-          'box index: $index',
+final WidgetGeneratorCallback generateColoredBox = (int index) => Center(
+      child: SizedBox(
+        height: 100,
+        width: 100,
+        child: ColoredBox(
+          color: const Color.fromRGBO(89, 222, 255, 1),
+          child: Text(
+            'box index: $index',
+          ),
         ),
       ),
     );
