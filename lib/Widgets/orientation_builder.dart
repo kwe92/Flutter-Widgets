218a043778396b75b6782421aab79be533b61b2a
Kwe <Kweayon@gmail.com>
2023-07-18 13:12:28 -0500

    update: expanded on lesson

--- lib/Widgets/orientation_builder.dart
+++ lib/Widgets/orientation_builder.dart
@@ -41,26 +41,50 @@ typedef WidgetGeneratorCallback = Widget Function(
   int index,
 );
 
-final WidgetGeneratorCallback generateColoredBox = (int index) => Center(
-      child: Container(
-        height: 185,
-        width: 185,
-        child: ColoredBox(
-          color: const Color.fromRGBO(188, 47, 230, 1),
-          child: Center(
-            child: FittedBox(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Text(
-                  'box index: $index',
-                  style: customTextStyle,
-                ),
-              ),
+final WidgetGeneratorCallback generateColoredBox = (int index) {
+  const double size = 185;
+  return Center(
+    child: Container(
+      height: size,
+      width: size,
+      child: _CoustomColoredBox(
+        index: index,
+      ),
+    ),
+  );
+};
+
+class _CustomDecoration extends BoxDecoration {
+  final double size;
+  const _CustomDecoration({required this.size});
+
+  @override
+  // TODO: implement borderRadius
+  BorderRadiusGeometry? get borderRadius => BorderRadius.circular(size / 2);
+}
+
+class _CoustomColoredBox extends StatelessWidget {
+  final int index;
+  const _CoustomColoredBox({required this.index, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: const Color.fromRGBO(188, 47, 230, 1),
+      child: Center(
+        child: FittedBox(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Text(
+              'box index: $index',
+              style: customTextStyle,
             ),
           ),
         ),
       ),
     );
+  }
+}
 
 const TextStyle customTextStyle = TextStyle(
   fontSize: 21,
