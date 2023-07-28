b46abc8420a666e605f8048c9aed8059e476a215
Kwe <Kweayon@gmail.com>
2023-07-28 16:03:34 -0500

    handle navigating to screen b

--- lib/Widgets/auto_route_guide/screens/screen_a.dart
+++ lib/Widgets/auto_route_guide/screens/screen_a.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/Widgets/auto_route_guide/routes/app_router.gr.dart';
 
 @RoutePage()
 class ScreenA extends StatelessWidget {
@@ -12,7 +13,28 @@ class ScreenA extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Screen A'),
           ),
-          body: Container(),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: ElevatedButton(
+                  onPressed: () {
+                    _handlePushScreenB(context);
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24.0),
+                    child: Text('Push to Screen B'),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       );
 }
+
+void _handlePushScreenB(BuildContext context) => AutoRouter.of(context).push(
+      const RouteB(),
+    );
