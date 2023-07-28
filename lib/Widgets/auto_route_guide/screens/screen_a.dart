3230fb452399d08520eb554fbf89e08de7082d47
Kwe <Kweayon@gmail.com>
2023-07-28 14:42:46 -0500

    update: updated screens with a basic UI and added @RoutePage annotation

--- lib/Widgets/auto_route_guide/screens/screen_a.dart
+++ lib/Widgets/auto_route_guide/screens/screen_a.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class ScreenA extends StatelessWidget {
   const ScreenA({super.key});
 
--- lib/Widgets/auto_route_guide/screens/screen_b.dart
+++ lib/Widgets/auto_route_guide/screens/screen_b.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class ScreenB extends StatelessWidget {
   const ScreenB({super.key});
 
