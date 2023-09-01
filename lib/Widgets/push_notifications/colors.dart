01e9bc26787ceb976d4df076dea331e8bd750124
Kwe <Kweayon@gmail.com>
2023-09-01 14:15:26 -0500

    update: implemented app colors

---
+++ lib/Widgets/push_notifications/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PushNotificationAppColors {
+  const PushNotificationAppColors._();
+  static const Color primary = Color(0xFF00569e);
+  static const Color secondary = Color(0xFF47c5fb);
+  static const Color backgroundColor = Color(0xFFFFd580);
+}
