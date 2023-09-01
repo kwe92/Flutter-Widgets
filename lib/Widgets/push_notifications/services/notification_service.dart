e578ce5782ca2e2d1e23cb6d1b34fd5eeaa31ad2
Kwe <Kweayon@gmail.com>
2023-09-01 15:58:24 -0500

    initial norification service

---
+++ lib/Widgets/push_notifications/services/notification_service.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  const NotificationService._();
+
+  static final _notifications = FlutterLocalNotificationsPlugin();
+
+  static Future _notificationDetails() async {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails(
+        "channel id",
+        "channel name",
+        channelDescription: "channel description",
+        importance: Importance.max,
+      ),
+      iOS: DarwinNotificationDetails(
+        categoryIdentifier: "plainCategory",
+      ),
+    );
+  }
+
+  static Future showNotification({
+    int id = 0,
+    String? title,
+    String? body,
+    String? payload,
+  }) async =>
+      _notifications.show(
+        id,
+        title,
+        body,
+        await _notificationDetails(),
+        payload: payload,
+      );
+}
