82ab4dde9b056c3fe45dd75f495f28f52d3c6009
Kwe <Kweayon@gmail.com>
2023-10-10 17:10:20 -0500

    update: initial implementation

---
+++ lib/Widgets/sliders/widgets/sliders/slider_color_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// TODO: Add comments
+
+class SliderColorWidget extends StatefulWidget {
+  const SliderColorWidget({super.key});
+
+  @override
+  State<SliderColorWidget> createState() => _SliderColorWidgetState();
+}
+
+class _SliderColorWidgetState extends State<SliderColorWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
