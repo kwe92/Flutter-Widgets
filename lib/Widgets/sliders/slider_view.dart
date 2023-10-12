ad1bf55fb29b0a8c97a882de5ace5f9efdfc0b7b
Kwe <Kweayon@gmail.com>
2023-10-12 13:45:47 -0500

    update: instantiated SliderColorGradient

--- lib/Widgets/sliders/slider_view.dart
+++ lib/Widgets/sliders/slider_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/slider_color_gradient.dart';
 import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/slider_color_widget.dart';
 import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/slider_label_widget.dart';
 import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/slider_size_widget.dart';
@@ -24,11 +25,18 @@ class SliderView extends StatelessWidget {
           icon: Icon(Icons.text_fields),
           text: "Labels",
         ),
+        Tab(
+          icon: Icon(Icons.border_color),
+          text: "Gradient",
+        ),
       ],
       children: [
         SliderColorWidget(),
         SliderSizeWidget(),
         SliderLabelWidget(),
+        SliderColorGradient(
+          swap: true,
+        ),
       ],
     );
   }
