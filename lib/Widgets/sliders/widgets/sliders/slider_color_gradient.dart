bcdfb2c48e5809b3bebb212a128b5ec985048454
Kwe <Kweayon@gmail.com>
2023-10-13 13:13:18 -0500

    refact: refactored SliderColorGradient from stateful to stateless

--- lib/Widgets/sliders/widgets/sliders/slider_color_gradient.dart
+++ lib/Widgets/sliders/widgets/sliders/slider_color_gradient.dart
@@ -2,26 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/gradient_track_shape.dart';
 import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/line_tickmark_shape.dart';
 import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/rectangular_slider_value_indicator_shape.dart';
+import 'package:flutter_widgets/Widgets/sliders/widgets/sliders/slider_color_gradient_model.dart';
+import 'package:provider/provider.dart';
 
-class SliderColorGradient extends StatefulWidget {
+class SliderColorGradient extends StatelessWidget {
   final bool swap;
+
+  final double min;
+
+  final double max;
+
+  final int divisions;
+
   const SliderColorGradient({
+    this.min = 0,
+    this.max = 10,
+    this.divisions = 10,
     this.swap = false,
     super.key,
   });
-
-  @override
-  State<SliderColorGradient> createState() => _SliderColorGradientState();
-}
-
-class _SliderColorGradientState extends State<SliderColorGradient> {
-  double value = 0;
   @override
   Widget build(BuildContext context) {
+    final SliderColorGradientModel model = context.watch<SliderColorGradientModel>();
     return SliderTheme(
       data: SliderThemeData(
         trackHeight: 24.0,
-        trackShape: GradientTrackShape(swap: widget.swap),
+        trackShape: GradientTrackShape(swap: swap),
         activeTrackColor: Colors.purple,
         inactiveTrackColor: Colors.red,
         thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 10),
@@ -38,56 +44,57 @@ class _SliderColorGradientState extends State<SliderColorGradient> {
           fontWeight: FontWeight.w700,
         ),
       ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Slider(
-            value: value,
-            min: 0,
-            max: 10,
-            divisions: 10,
-            label: value.toInt().toString(),
-            onChanged: (value) => setState(
-              () => this.value = value,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Slider(
+              value: model.value,
+              min: min,
+              max: max,
+              divisions: divisions,
+              label: model.value.toInt().toString(),
+              onChanged: (value) => model.setValue(value),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 11, right: 7),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '0',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: !widget.swap
-                        ? const Color(
-                            0xff10883D,
-                          )
-                        : const Color(
-                            0xff993218,
-                          ),
+            Padding(
+              padding: const EdgeInsets.only(left: 11, right: 7),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      color: !swap
+                          ? const Color(
+                              0xff10883D,
+                            )
+                          : const Color(
+                              0xff993218,
+                            ),
+                    ),
                   ),
-                ),
-                Text(
-                  '10',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: !widget.swap
-                        ? const Color(
-                            0xff993218,
-                          )
-                        : const Color(
-                            0xff10883D,
-                          ),
+                  Text(
+                    '10',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      color: !swap
+                          ? const Color(
+                              0xff993218,
+                            )
+                          : const Color(
+                              0xff10883D,
+                            ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
