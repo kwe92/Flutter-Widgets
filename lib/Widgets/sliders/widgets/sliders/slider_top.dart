2d0acd9a74cef8c2da8d830783f7f56c5ee99cb4
Kwe <Kweayon@gmail.com>
2023-10-14 14:11:02 -0500

    update: implemented SliderTop as its own widget

---
+++ lib/Widgets/sliders/widgets/sliders/slider_top.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/Widgets/sliders/utils.dart';
+
+// TODO: Add Comments
+
+class SliderTop extends StatelessWidget {
+  final List<String> labels;
+  final double indexTop;
+  final double min;
+  final double max;
+  final int divisions;
+  final Function(double)? onChanged;
+  final bool visibleLabel;
+
+  const SliderTop({
+    required this.labels,
+    required this.indexTop,
+    required this.min,
+    required this.max,
+    required this.divisions,
+    required this.onChanged,
+    this.visibleLabel = false,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: Utils.modelBuilder(
+                  labels,
+                  Utils.labelBuilder(
+                    indexTop,
+                    visible: true,
+                  )),
+            ),
+          ),
+          Slider(
+            value: indexTop.toDouble(),
+            min: min,
+            max: max,
+            divisions: divisions,
+            label: visibleLabel ? labels[indexTop.toInt()] : null,
+            onChanged: onChanged,
+          ),
+        ],
+      );
+}
