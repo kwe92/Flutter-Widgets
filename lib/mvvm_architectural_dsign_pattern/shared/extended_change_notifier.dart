5dff667eccb02a201718ee2c2be2d657332b374e
Kwe <Kweayon@gmail.com>
2023-12-30 11:36:55 -0600

    feat: implemented ExtendedChangeNotifier to add loading state handling

---
+++ lib/mvvm_architectural_dsign_pattern/shared/extended_change_notifier.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/material.dart';
+
+/// ChangeNotifier with added loading state handling.
+class ExtendedChangeNotifier extends ChangeNotifier {
+  bool _isLoading = false;
+
+  /// Represents the loading state of the ViewModel.
+  get isLoading => _isLoading;
+
+  /// Sets the loading state of the ViewModel, calls notifiy listeners.
+  void setIsLoading(bool isBusy) {
+    _isLoading = isBusy;
+    notifyListeners();
+  }
+}
