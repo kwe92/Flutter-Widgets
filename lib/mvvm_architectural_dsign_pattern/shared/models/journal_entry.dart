f3ea07a2a5ccdb42013bfb2a3b6c45846a4f3958
Kwe <Kweayon@gmail.com>
2023-12-30 11:31:21 -0600

    feat: concrete implementation of BaseJournalEntry

---
+++ lib/mvvm_architectural_dsign_pattern/shared/models/journal_entry.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:flutter_widgets/mvvm_architectural_dsign_pattern/shared/models/base_journal_entry.dart';
+
+/// Represents the Domain Model and DTO (Data Transfer Object) for journal entries.
+class JournalEntry extends BaseJournalEntry {
+  const JournalEntry({
+    super.entryId,
+    super.userId,
+    super.content,
+    super.createdAt,
+    super.updatedAt,
+  });
+
+  @override
+  String toJSON() => jsonEncode(toMap());
+
+  @override
+  Map<String, dynamic> toMap() => {
+        'entry_id': entryId ?? 0,
+        'user_id': userId ?? '',
+        'content': content ?? '',
+        'created_at': createdAt ?? DateTime.now(),
+        'updated_at': updatedAt ?? DateTime.now(),
+      };
+
+  @override
+  String toString() {
+    return 'JournalEntry(entryId: $entryId, userId: $userId, content: $content, createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+}
+
+
+// Model Class
+
+//   - The Model Class in M-V-VM (Model-View-ViewModel) Architectural Design Pattern
+//   - Represents the Domain Model that encapsulates your applications data, part of Domain Driven Design
+//   - The Model Class also serves as a DTO (Data Transfer Object) facilitating communication between API's and your application
+//   - The communication mechanism consists mainly of marshaling (serialization) and unmarshaling (deserialization, can be handed off to a Factory Class) of the domain
+//     model to and from JSON respectively
+//   - Model Classes are typicall immutable
