24df706409333716eb5c22bbcd8a589cf9d100f4
Kwe <Kweayon@gmail.com>
2024-01-01 13:29:12 -0600

    feat: updated comments

--- lib/mvvm_architectural_dsign_pattern/shared/services/get_it.dart
+++ lib/mvvm_architectural_dsign_pattern/shared/services/get_it.dart
@@ -5,13 +5,39 @@ import 'package:get_it/get_it.dart';
 
 final GetIt locator = GetIt.I;
 
-/// register global dependencies used throughout your application
+/// register global dependencies used throughout your application.
 void configureDependencies() {
   locator.registerLazySingleton(() => JournalEntryService());
 }
 
-// TODO: add comments
-
 // Why Use Services and a Service Locator?
 
-//   - 
+//   - removes application and business logic (separation of concerns) from your View (Widgets/ U.I. / Presentation layer)
+//     and ViewModel (mutable presentation logic) classes
+//   - if you are implementing the M-V-VM Architectural Design Pattern
+//   - removes direct dependencies (the amount of imports a modules has)
+//     allowing code to be organized, maintainable,scalable and testable (mocking is easier)
+
+//   get_it Service Locator
+
+//     - a service locator package for Dart and Flutter projects inspired by .Net Splat
+//     - GetIt is a Singleton which means you are using the same instance throughout your application
+//     - register and create instances in constant time O(1)
+//     - can use Dependency Inversion to return concrete implementations based on abstract contracts
+//       using an abstract base class as a registration type e.g.:
+
+//         ~ GetIt.instance.registerSingleton<RegistrationType>(ConcreteImplementationOfRegistrationType)
+//         ~ GetIt.instance.registerSingleton<AbstractClass>(ConcreteImplementationOfAbstractClass())
+
+
+//     - GetIt is best used by assigning a GetIt.instance (or GetIt.I for brevity)
+//       to a global (euphemistic ambient) variable
+//     - you can name the variable whatever you want (`locator` is my choosen name for my GetIt service locator variable)
+
+// How to Use get_it Service Locator
+
+//   1: assign GetIt.instance to global variable
+//   2.a: implement a function to register services to GetIt.instance variable (i name the function configureDependencies)
+//   2.b: insure your dependency/service registration function is called within your startup code as high as possible
+//   3: access registered objects by calling locator.get<RegisteredConcreteImplementation>()
+//     - I prefer to access registered objects by using global getter method computed variables
