4a21818d484a49d2d85a1893253eee8c7c4ede14
Kwe <Kweayon@gmail.com>
2024-01-02 22:02:03 -0600

    refact: updated comments

--- lib/mvvm_architectural_dsign_pattern/shared/services/get_it.dart
+++ lib/mvvm_architectural_dsign_pattern/shared/services/get_it.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_widgets/mvvm_architectural_dsign_pattern/shared/services/journal_entry_service.dart';
 import 'package:get_it/get_it.dart';
 
-// module responsible for registering global dependencies as Singleton's and Factoy's
+// module responsible for registering global dependencies as Singleton's and Factoy's (not to be confused with the creational design pattern)
 
 final GetIt locator = GetIt.I;
 
-/// register global dependencies used throughout your application.
+/// register global services (dependencies) used throughout your application.
 void configureDependencies() {
   locator.registerLazySingleton(() => JournalEntryService());
 }
@@ -16,7 +16,7 @@ void configureDependencies() {
 //     and ViewModel (mutable presentation logic) classes
 //   - if you are implementing the M-V-VM Architectural Design Pattern
 //   - removes direct dependencies (the amount of imports a modules has)
-//     allowing code to be organized, maintainable,scalable and testable (mocking is easier)
+//     allowing code to be organized, maintainable, scalable and testable (mocking is easier)
 
 //   get_it Service Locator
 
@@ -26,18 +26,22 @@ void configureDependencies() {
 //     - can use Dependency Inversion to return concrete implementations based on abstract contracts
 //       using an abstract base class as a registration type e.g.:
 
-//         ~ GetIt.instance.registerSingleton<RegistrationType>(ConcreteImplementationOfRegistrationType)
+//         ~ GetIt.instance.registerSingleton<RegistrationType>(ConcreteImplementationOfRegistrationType())
 //         ~ GetIt.instance.registerSingleton<AbstractClass>(ConcreteImplementationOfAbstractClass())
-
 
 //     - GetIt is best used by assigning a GetIt.instance (or GetIt.I for brevity)
 //       to a global (euphemistic ambient) variable
-//     - you can name the variable whatever you want (`locator` is my choosen name for my GetIt service locator variable)
+//     - you can name the variable whatever you want (`locator` is the choosen name I use for my GetIt service locator variable)
 
 // How to Use get_it Service Locator
 
 //   1: assign GetIt.instance to global variable
-//   2.a: implement a function to register services to GetIt.instance variable (i name the function configureDependencies)
+//   2.a: implement a function to register services to GetIt.instance variable (`configureDependencies` is the choosen name I use for my register function)
 //   2.b: insure your dependency/service registration function is called within your startup code as high as possible
 //   3: access registered objects by calling locator.get<RegisteredConcreteImplementation>()
-//     - I prefer to access registered objects by using global getter method computed variables
+//     - I prefer to access registered objects by using global getter method computed variables as shown bellow:
+
+// note: there should not be a leading unerscore, was placed there to not have duplicative global variable names
+JournalEntryService get _journalEntryService {
+  return locator.get<JournalEntryService>();
+}
