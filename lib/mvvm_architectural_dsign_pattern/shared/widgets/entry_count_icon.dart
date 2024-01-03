4e18ab4b21d42c93f6e250ed4d092d783e00f1dc
Kwe <Kweayon@gmail.com>
2024-01-02 22:02:40 -0600

    refact: updated comments

--- lib/mvvm_architectural_dsign_pattern/shared/widgets/entry_count_icon.dart
+++ lib/mvvm_architectural_dsign_pattern/shared/widgets/entry_count_icon.dart
@@ -10,20 +10,32 @@ class EntryCountIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<JournalEntryService>.value(
       value: journalEntryService,
-      child: Consumer<JournalEntryService>(
-        builder: (BuildContext context, JournalEntryService journalEntryService, _) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.book_online_outlined),
-              const SizedBox(
-                width: 6,
-              ),
-              Text(journalEntryService.journalEntries.length.toString()),
-            ],
-          );
-        },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.book_online_outlined),
+          const SizedBox(
+            width: 6,
+          ),
+          // TODO: ensure U.I. rebuilds when an entry is removed
+          Text(context.watch<JournalEntryService>().journalEntries.length.toString()),
+        ],
       ),
+
+      // Consumer<JournalEntryService>(
+      //   builder: (BuildContext context, JournalEntryService journalEntryService, _) {
+      //     return Row(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         const Icon(Icons.book_online_outlined),
+      //         const SizedBox(
+      //           width: 6,
+      //         ),
+      //         Text(journalEntryService.journalEntries.length.toString()),
+      //       ],
+      //     );
+      //   },
+      // ),
     );
   }
 }
@@ -31,9 +43,9 @@ class EntryCountIcon extends StatelessWidget {
 // ChangeNotifierProvider<T extends ChangeNotifier?>
 
 //   - creates or reuses a ChangeNotifier depending on which constructor you use
-//   = there are two constructors:
-//       - 1: ChangeNotifierProvider() - (default constructor used to create a new instance of ChangeNotifier)
-//       - 2: ChangeNotifierProvider<T>.value - (named constructor used to provide an exisiting (instantiated) ChangeNotifier)
-//   - listen and expose a ChangeNotifier to decendant nodes (Widgets) in the Widget tree
+//   - there are two constructors:
+//       - 1: ChangeNotifierProvider() - (the default constructor used to create a new instance of a ChangeNotifier concrete implementation)
+//       - 2: ChangeNotifierProvider<T>.value - (a named constructor used to provide an exisiting (instantiated) ChangeNotifier implementation)
+//   - listens to and exposes a ChangeNotifier to decendant nodes (Widgets) in the Widget tree
 //   - decendant nodes (Widgets) dependent upon the ChangeNotifier exposed by the ancestor node (ChangeNotifierProvider)
 //     rebuild their U.I. with updated data when a notification event is emited by ChangeNotifier.notifyListeners
