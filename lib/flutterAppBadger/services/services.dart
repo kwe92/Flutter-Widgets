835dc547cdad76b0ff71882aaa33db783a3c123b
Kwe <Kweayon@gmail.com>
2024-01-09 23:36:57 -0600

    feat: FlutterAppBadgerService

---
+++ lib/flutterAppBadger/services/services.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_widgets/flutterAppBadger/services/flutter_app_badger_service.dart';
+import 'package:flutter_widgets/flutterAppBadger/services/get_it.dart';
+
+FlutterAppBadgerService get flutterAppBadgerService => flutterBadgerLocator.get<FlutterAppBadgerService>();
