04ebb0a352ff758552413bc271977b7dc63dc11f
Kwe <Kweayon@gmail.com>
2024-01-16 12:57:05 -0600

    feat: implemented clearQueriedItems

--- lib/listViewSearchBar/searchable_list_view_model.dart
+++ lib/listViewSearchBar/searchable_list_view_model.dart
@@ -2,8 +2,38 @@
 
 import 'package:flutter/material.dart';
 
-class SearchableListViewModel extends ChangeNotifier {
-  var _items = List<String>.generate(10000, (index) => 'item: $index');
+// TODO: refact to not be static
 
-  List<String> get items => _items;
+class SearchableListViewModel extends ChangeNotifier {
+  static var _items = List<String>.generate(10000, (index) => 'item: ${index + 1}');
+
+  List<String> _queriedItems = _items;
+
+  String _query = '';
+
+  String get query => _query;
+
+  List<String> get queriedItems => _queriedItems;
+
+  void onQueryItems(String query) {
+    _setQuery(query);
+    _queriedItems = _filterItems(query);
+    notifyListeners();
+  }
+
+  void clearQueriedItems() {
+    _queriedItems = _items;
+    notifyListeners();
+  }
+
+  void _setQuery(String query) {
+    _query = query;
+    notifyListeners();
+  }
+
+  List<String> _filterItems(String query) => _items
+      .where(
+        (item) => item.toLowerCase().contains(query.toLowerCase()),
+      )
+      .toList();
 }
