81564378fd080950572425408b7bebd516088440
Kwe <Kweayon@gmail.com>
2024-01-16 12:55:22 -0600

    feat: implemented search bar implemented

--- lib/listViewSearchBar/searchable_list_view.dart
+++ lib/listViewSearchBar/searchable_list_view.dart
@@ -17,69 +17,93 @@ class SearchableListView extends StatelessWidget {
 
           const horizontalPadding = 16.0;
 
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'Searchable List',
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            body: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: horizontalPadding, top: 16, right: horizontalPadding),
-                  child: TextField(
-                    controller: searchController,
-                    onChanged: (value) {},
-                    decoration: const InputDecoration(
-                      labelText: 'Search',
-                      hintText: 'Search',
-                      prefixIcon: Icon(Icons.search_outlined),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(50),
-                        ),
-                      ),
-                    ),
+          return SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text(
+                  'Searchable List',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: model.items.length,
-                    itemBuilder: (context, index) => Padding(
-                      padding: EdgeInsets.only(
-                        left: horizontalPadding,
-                        top: index == 0 ? 16 : 0,
-                        right: horizontalPadding,
-                        bottom: index != model.items.length - 1 ? 12 : 0,
-                      ),
-                      child: ListTile(
-                        shape: const StadiumBorder(),
-                        tileColor: Theme.of(context).colorScheme.surface,
-
-                        // tileColor: Colors.purple.withOpacity(0.50),
-                        title: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 12),
-                          child: Text(
-                            model.items[index],
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
-                            ),
+              ),
+              body: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: horizontalPadding, top: 24, right: horizontalPadding),
+                    // SEARCH BAR
+                    child: TextField(
+                      controller: searchController,
+                      onChanged: model.onQueryItems,
+                      decoration: InputDecoration(
+                        labelText: 'Search',
+                        hintText: 'Search',
+                        prefixIcon: const Icon(Icons.search_outlined),
+                        suffixIcon: model.query.isNotEmpty
+                            ? IconButton(
+                                onPressed: () {
+                                  searchController.clear();
+                                  model.clearQueriedItems();
+                                },
+                                icon: const Icon(Icons.clear),
+                              )
+                            : null,
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(50),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  // LIST VIEW
+                  Expanded(
+                    child: model.queriedItems.isEmpty
+                        ? Padding(
+                            padding: EdgeInsets.only(
+                              left: horizontalPadding,
+                              top: MediaQuery.of(context).size.height / 4.5,
+                              right: horizontalPadding,
+                            ),
+                            child: Text(
+                              'There are no items containing the text: ${model.query}',
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                            ),
+                          )
+                        : ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: model.queriedItems.length,
+                            itemBuilder: (context, index) => Padding(
+                              padding: EdgeInsets.only(
+                                left: horizontalPadding,
+                                top: index == 0 ? 16 : 0,
+                                right: horizontalPadding,
+                                bottom: index != model.queriedItems.length - 1 ? 12 : 0,
+                              ),
+                              child: ListTile(
+                                shape: const StadiumBorder(),
+                                tileColor: Theme.of(context).colorScheme.surface,
+                                title: Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 12),
+                                  child: Text(
+                                    model.queriedItems[index],
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                  ),
+                ],
+              ),
             ),
           );
         },
