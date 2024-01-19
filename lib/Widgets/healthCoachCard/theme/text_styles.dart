f44d9077c5416d46ec0a40203286ba7b1f581d1c
Kwe <Kweayon@gmail.com>
2024-01-19 16:57:24 -0600

    feat: text styles

---
+++ lib/Widgets/healthCoachCard/theme/text_styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/Widgets/healthCoachCard/theme/colors.dart';
+
+TextStyle textStyleHeader1 = const TextStyle(fontWeight: FontWeight.w600, fontSize: 24, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleHeader2 = const TextStyle(fontWeight: FontWeight.w600, fontSize: 20, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleHeader3 = const TextStyle(fontWeight: FontWeight.w600, fontSize: 16, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleHeader4 = const TextStyle(fontWeight: FontWeight.w700, fontSize: 40, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleHeader5 = const TextStyle(fontWeight: FontWeight.w700, fontSize: 32, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleHeader6 = const TextStyle(fontWeight: FontWeight.w700, fontSize: 100, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleHeader7 = const TextStyle(fontWeight: FontWeight.w400, fontSize: 18, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleHeader8 = const TextStyle(fontWeight: FontWeight.w400, fontSize: 18, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleBody1 = const TextStyle(fontWeight: FontWeight.w400, fontSize: 16, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleBody2 = const TextStyle(fontWeight: FontWeight.w400, fontSize: 14, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleSubtitle1 = const TextStyle(fontWeight: FontWeight.w400, fontSize: 16, color: CareNavigationColors.tpDarkBlue);
+TextStyle buttonText = const TextStyle(fontWeight: FontWeight.w600, fontSize: 18, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleDialogHeader = const TextStyle(fontWeight: FontWeight.w800, fontSize: 13, color: CareNavigationColors.gray);
+TextStyle textStyleCardHeader = const TextStyle(fontWeight: FontWeight.w800, fontSize: 13, color: CareNavigationColors.gray);
+TextStyle textStyleCardTitle = const TextStyle(fontWeight: FontWeight.w400, fontSize: 14, color: CareNavigationColors.tpDarkBlue);
+TextStyle textStyleInsideProgressWidget =
+    const TextStyle(fontWeight: FontWeight.w700, fontSize: 10, height: 1, color: CareNavigationColors.tpBrightBlue);
+
+TextStyle fifteen700 = const TextStyle(fontWeight: FontWeight.w700, fontSize: 15);
+TextStyle fourteen700 = const TextStyle(fontWeight: FontWeight.w700, fontSize: 14);
+TextStyle fifteen500 = const TextStyle(fontWeight: FontWeight.w500, fontSize: 15);
+TextStyle twentyFour400 = const TextStyle(fontWeight: FontWeight.w400, fontSize: 24);
+
+TextStyle textStyleBlueHeader1 = textStyleHeader1.copyWith(color: CareNavigationColors.tpBlue);
+TextStyle textStyleBlueHeader2 = textStyleHeader2.copyWith(color: CareNavigationColors.tpBlue);
+TextStyle textStyleBlueHeader3 = textStyleHeader3.copyWith(color: CareNavigationColors.tpBlue);
+TextStyle textStyleBlueHeaderNew = fifteen700.copyWith(color: CareNavigationColors.tpBrightBlue);
+TextStyle textStyleBlueBody1 = textStyleBody1.copyWith(color: CareNavigationColors.tpBlue);
+TextStyle textStyleBlueBody2 = textStyleBody2.copyWith(color: CareNavigationColors.tpBlue);
+TextStyle textStyleBlueSubtitle1 = textStyleSubtitle1.copyWith(color: CareNavigationColors.tpBlue);
