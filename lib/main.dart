0a5507f58fce8657bf0238e9aea03eb95a26e7c0
Kwe <Kweayon@gmail.com>
2024-02-10 15:37:42 -0600

    feat: misc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/Widgets/quotes_feature/ui/quotes_view.dart';
 import 'package:flutter_widgets/Widgets/table_calendar/complex_example/calendar_complex_example.dart';
 import 'package:flutter_widgets/Widgets/table_calendar/events_example/calendar_event_example_view.dart';
 import 'package:flutter_widgets/Widgets/table_calendar/journal_date_view.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuotesView(),
+  ));
+}
 
 // void main() {
 //   runApp(const MaterialApp(
@@ -9,11 +16,11 @@ import 'package:flutter_widgets/Widgets/table_calendar/journal_date_view.dart';
 //   ));
 // }
 
-void main() {
-  runApp(const MaterialApp(
-    home: TableEventsExampleView(),
-  ));
-}
+// void main() {
+//   runApp(const MaterialApp(
+//     home: TableEventsExampleView(),
+//   ));
+// }
 
 // void main() {
 //   runApp(MaterialApp(
