a5612e1d77255e365bd1e07533f4933753b07377
Kwe <Kweayon@gmail.com>
2024-03-01 23:04:21 -0600

    feat: helper functions and classes

---
+++ lib/fl_charts/utils.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:flutter_widgets/fl_charts/mood.dart';
+
+class GetRandom {
+  GetRandom._();
+
+  static final rng = Random();
+
+  /// exclusive range
+  static int randRangeInt({int min = 0, required int max}) => rng.nextInt(max - min) + min;
+}
+
+String getRandomMood() {
+  final int value = GetRandom.randRangeInt(min: 1, max: 6);
+
+  return switch (value) {
+    1 => MoodType.awesome.text,
+    2 => MoodType.happy.text,
+    3 => MoodType.okay.text,
+    4 => MoodType.bad.text,
+    5 => MoodType.terrible.text,
+    int() => "",
+  };
+}
+
+DateTime getRandomDateTime() {
+  final int value = GetRandom.randRangeInt(min: 1, max: 31);
+
+  return DateTime.now().add(Duration(days: -value));
+}
