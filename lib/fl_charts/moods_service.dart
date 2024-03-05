0b22ed732cd67a8cd712ecc38473bd04fb7aa841
Kwe <Kweayon@gmail.com>
2024-03-04 18:01:59 -0600

    feat: initial mood service

---
+++ lib/fl_charts/moods_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/fl_charts/mood.dart';
+import 'package:flutter_widgets/fl_charts/utils.dart';
+
+final moodsService = MoodsService();
+
+class MoodsService extends ChangeNotifier {
+  List<WeightedMood> _weightedMoodData = [];
+
+  List<WeightedMood> get weightedMoodData => _weightedMoodData;
+
+  MoodsService() {
+    _weightedMoodData = [
+      for (int i = 0; i < 90; i++)
+        WeightedMood(
+          mood: getRandomMood(),
+          createdAt: getRandomDateTime(),
+        )
+    ];
+  }
+}
