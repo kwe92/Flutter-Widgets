f2a3dd09c9d7a130c40f6b0f12a75c1d58726eae
Kwe <Kweayon@gmail.com>
2024-03-20 21:42:11 -0500

    feat: implemented week and month view

--- lib/fl_charts/line_chart_example_view.dart
+++ lib/fl_charts/line_chart_example_view.dart
@@ -1,6 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/fl_charts/charts.dart';
+import 'package:flutter_widgets/fl_charts/chartsv2.dart';
 import 'package:flutter_widgets/fl_charts/line_chart_example_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -26,7 +26,7 @@ class LineChartSampleView extends StatelessWidget {
                   const Text(
                     'Mood Statistics',
                     style: TextStyle(
-                      color: AppColors.primary,
+                      color: Colors.lightBlue,
                       fontSize: 32,
                       fontWeight: FontWeight.bold,
                       letterSpacing: 2,
@@ -39,15 +39,16 @@ class LineChartSampleView extends StatelessWidget {
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.only(right: 16, left: 6),
-                      child: LineChartExample(
+                      child: LineChartExampleV2(
                         spots: [
                           for (int i = 0; i < model.groupedMoodsData.length; i++)
                             () {
+                              print("model.groupedMoodsData.length: ${model.groupedMoodsData.length}");
                               final entry = model.groupedMoodsData[i].entries.toList()[0];
 
                               debugPrint("entry: $entry");
 
-                              return FlSpot(entry.key.toDouble(), entry.value * 4);
+                              return FlSpot(i + 1.0, entry.value * 4);
                             }()
                         ],
                       ),
