3ab64a9b94bc2c24e70ed30eb246f7263b3628af
Kwe <Kweayon@gmail.com>
2024-04-04 08:58:28 -0500

    feat: misc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/awesome_notifications/awesomeNotificationExampleView.dart';
+import 'package:flutter_widgets/complex_layout/complex_layout_view.dart';
+import 'package:flutter_widgets/offline_database_example_notes_app/app_navigatior.dart';
+import 'package:flutter_widgets/offline_database_example_notes_app/features/notes/ui/notes_view.dart';
+import 'package:flutter_widgets/offline_database_example_notes_app/theme/theme.dart';
 
 void main() async {
   runApp(
-    const MaterialApp(
-      home: AwesomeNotificationExampleView(),
+    MaterialApp(
+      navigatorKey: AppNavigatior.navigatorKey,
+      home: const NotesView(),
+      theme: AppThemeNotes.getTheme(),
+      // home: ComplexLayoutExample(),
+
+      // home: AwesomeNotificationExampleView(),
     ),
   );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import flutter_app_badger
 import flutter_local_notifications
 import path_provider_foundation
 import share_plus
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
@@ -25,4 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
