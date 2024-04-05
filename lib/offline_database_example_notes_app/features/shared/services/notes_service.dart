42b86a9037dd540be2d764a28de0949069eceec5
Kwe <Kweayon@gmail.com>
2024-04-05 11:51:18 -0500

    feat: notes ervice

---
+++ lib/offline_database_example_notes_app/features/shared/services/notes_service.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/offline_database_example_notes_app/features/notes/models/note.dart';
+import 'package:flutter_widgets/offline_database_example_notes_app/features/notes/models/notes_provider.dart';
+
+final notesService = NotesService();
+
+class NotesService extends ChangeNotifier {
+  List<Note?>? _notes;
+
+  List<Note?>? get notes => _notes;
+
+  Future<void> getNotes() async {
+    _notes = await notesProviderService.getAllNotes();
+    notifyListeners();
+  }
+
+  void addNote(Note note) {
+    _notes!.add(note);
+    debugPrint("Note added successfully.");
+    notifyListeners();
+  }
+
+  void deleteNote(Note note) {
+    _notes!.remove(note);
+    debugPrint("Note deleted successfully.");
+    notifyListeners();
+  }
+
+  void replaceNote(Note note, Note prevNote) {
+    final index = _notes!.indexOf(prevNote);
+
+    _notes!.remove(prevNote);
+
+    _notes!.insert(index, note);
+
+    debugPrint("Note updates successfully.");
+    notifyListeners();
+  }
+}
