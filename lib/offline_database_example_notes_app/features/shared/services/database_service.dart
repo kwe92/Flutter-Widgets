9da5ed9af78485e693ad68b7c91582fccf4cbf0a
Kwe <Kweayon@gmail.com>
2024-04-12 19:13:06 -0500

    feat: cleaned up DatabaseService specifications

--- lib/offline_database_example_notes_app/features/shared/services/database_service.dart
+++ lib/offline_database_example_notes_app/features/shared/services/database_service.dart
@@ -5,7 +5,10 @@ import 'package:sqflite/sqflite.dart';
 
 final databaseService = DatabaseService();
 
-// TODO: add explanatory comments
+/// Responsible for:
+/// - database initialization, creation, and migration
+/// - providing an instance of the database
+/// - database opening and closing
 
 class DatabaseService extends ChangeNotifier {
   late Database _db;
@@ -15,62 +18,14 @@ class DatabaseService extends ChangeNotifier {
   ({
     String notes,
     String images,
-  }) tables = (notes: "Notes", images: "Images");
-
-  static Future<void> _createNotesTable(Database db) async {
-    await db.execute('DROP TABLE IF EXISTS Notes');
-
-    await db.execute(
-      '''CREATE TABLE 
-          Notes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT,
-            content TEXT,
-            created_at TEXT,
-            updated_at TEXT
-            )''',
-    );
-  }
-
-  static Future<void> _createImagesTable(Database db) async {
-    await db.execute('DROP TABLE IF EXISTS Images');
-
-    await db.execute(
-      '''CREATE TABLE 
-          Images (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            note_id INTEGER,
-            image_name TEXT,
-            FOREIGN KEY (note_id) REFERENCES Notes(id) ON DELETE CASCADE
-            )''',
-    );
-  }
+  }) tables = (notes: 'Notes', images: 'Images');
 
   Future<void> initialize() async {
-    final directory = await getApplicationDocumentsDirectory();
+    final String path = await _getPath('offline_database');
 
-    final path = join(directory.path, 'offline_database');
+    _db = await _openDatabase(3, path);
 
-    _db = await openDatabase(
-      version: 3,
-      path,
-      onCreate: (db, version) async {
-        await _createNotesTable(db);
-
-        await _createImagesTable(db);
-      },
-      onUpgrade: (db, oldVersion, newVersion) async {
-        if (oldVersion == 2) {
-          await _createImagesTable(db);
-        }
-      },
-      onConfigure: (db) async {
-        // turn on the use of foreign key constraints | required
-        await db.execute('PRAGMA foreign_keys = ON');
-      },
-    );
-
-    await getCurrentSchema();
+    await _getCurrentSchemaInformation(_db);
 
     debugPrint("database opened successfully.");
   }
@@ -79,26 +34,84 @@ class DatabaseService extends ChangeNotifier {
     await db.close();
     debugPrint("database closed successfully.");
   }
+}
 
-  Future<void> getCurrentSchema() async {
-    final currentSchema = await db.query(
-      'sqlite_master',
-      columns: ['sql'],
-    );
+Future<Database> _openDatabase(int version, String path) async {
+  return openDatabase(
+    version: version, // latest version 3
+    path,
+    onCreate: _createDatabase,
+    onUpgrade: _migrateDatabase,
+    onConfigure: _configureDatabase,
+  );
+}
 
-    debugPrint('Current Schema:\n');
-    for (int i = 0; i < currentSchema.length; i++) {
-      debugPrint('${currentSchema[i]['sql']}\n');
-    }
+Future<void> _createDatabase(Database db, int version) async {
+  await _createNotesTable(db);
 
-// ! Remove
+  await _createImagesTable(db);
+}
 
-    final images = await db.query(
-      'Images',
-    );
-    debugPrint("images in database: ${images.length}");
+Future<void> _migrateDatabase(Database db, int oldVersion, int newVersion) async {
+  if (oldVersion == 2) {
+    await _createImagesTable(db);
+  }
+}
 
-// ! Remove end
+Future<void> _configureDatabase(Database db) async {
+  // turn on the use of foreign key constraints | required configuration to use foreign keys
+  await db.execute('PRAGMA foreign_keys = ON');
+}
+
+Future<void> _createNotesTable(Database db) async {
+  await db.execute('DROP TABLE IF EXISTS Notes');
+
+  await db.execute(
+    '''CREATE TABLE 
+          Notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            content TEXT,
+            created_at TEXT,
+            updated_at TEXT
+            )''',
+  );
+}
+
+Future<void> _createImagesTable(Database db) async {
+  await db.execute('DROP TABLE IF EXISTS Images');
+
+  await db.execute(
+    '''CREATE TABLE 
+          Images (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            note_id INTEGER,
+            image_name TEXT,
+            FOREIGN KEY (note_id) REFERENCES Notes(id) ON DELETE CASCADE
+            )''',
+  );
+}
+
+Future<String> _getPath(String databaseName) async {
+  // the platform specific directory
+  final directory = await getApplicationDocumentsDirectory();
+
+  // path where the database file will be saved
+  final path = join(directory.path, databaseName);
+
+  return path;
+}
+
+Future<void> _getCurrentSchemaInformation(Database db) async {
+  final currentSchema = [
+    ...await db.query('sqlite_master', columns: ['sql'])
+  ];
+
+  currentSchema.removeWhere((Map result) => result['sql'] == 'CREATE TABLE sqlite_sequence(name,seq)');
+
+  debugPrint('Current Schema:\n');
+  for (int i = 0; i < currentSchema.length; i++) {
+    debugPrint('${currentSchema[i]['sql']}\n');
   }
 }
 
@@ -106,11 +119,10 @@ class DatabaseService extends ChangeNotifier {
 
 //   - previous version numbers: [1]
 
-
-    // openDatabase(
-    //   path,
-    //   version: 1,
-    //   onCreate: (db, version) {
-    //     return db.execute("CREATE TABLE Notes(id INTEGER PRIMARY KEY, title TEXT, content TEXT, created_at TEXT)");
-    //   },
-    // );
+// openDatabase(
+//   path,
+//   version: 1,
+//   onCreate: (db, version) {
+//     return db.execute("CREATE TABLE Notes(id INTEGER PRIMARY KEY, title TEXT, content TEXT, created_at TEXT)");
+//   },
+// );
