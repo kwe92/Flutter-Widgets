508faf911a3d843031e8201b29bbe339268ee01e
Kwe <Kweayon@gmail.com>
2024-06-21 11:41:06 -0500

    feat: added icons and initial InfoListTile implementation

--- lib/ui_examples/travel_app/ui/location_detail_view.dart
+++ lib/ui_examples/travel_app/ui/location_detail_view.dart
@@ -28,6 +28,7 @@ class LocationDetailView extends StatelessWidget {
             ),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Padding(
                 padding: const EdgeInsets.only(
@@ -46,7 +47,11 @@ class LocationDetailView extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                child: InfoListTile(destination: "Andes Mountain", continent: "Soutth, America", price: 230),
+              ),
             ],
           ),
         ),
@@ -76,6 +81,104 @@ class CircularIconButtton extends StatelessWidget {
       ),
       child: Center(
         child: icon,
+      ),
+    );
+  }
+}
+
+class InfoListTile extends StatelessWidget {
+  final String destination;
+  final String continent;
+  final double price;
+
+  const InfoListTile({
+    required this.destination,
+    required this.continent,
+    required this.price,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // margin: const EdgeInsets.symmetric(horizontal: 20),
+      width: double.maxFinite,
+      height: 104,
+      decoration: BoxDecoration(
+        color: const Color(0xff132c40),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.15),
+            offset: const Offset(0, 5),
+            blurRadius: 9,
+          )
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8, top: 18, bottom: 18),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  destination,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 22,
+                  ),
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      "/Users/kwe/flutter-projects/FlutterWidgets/flutter_widgets/assets/location_icon.svg",
+                      width: 20,
+                      height: 20,
+                      color: const Color(0xffCAC8C8),
+                    ),
+                    const SizedBox(width: 6),
+                    Text(
+                      continent,
+                      style: const TextStyle(
+                        color: Color(0xffCAC8C8),
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 26, bottom: 16, right: 8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Price",
+                  style: TextStyle(
+                    color: Color(0xffCAC8C8),
+                    fontSize: 14,
+                  ),
+                ),
+                Text(
+                  "\$${price.toStringAsFixed(0)}",
+                  style: const TextStyle(
+                    color: Color(0xffCAC8C8),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 22,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
